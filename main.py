f49210e96c53ff1f96adbe3dff23029cb7e6b570
sidizawi <sidi.zawi@ulb.be>
2019-10-20 20:14:38 +0200

    Initial commit & Add matrix()

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+author : Zawi, Sidi M.
+github : sidizawi
+"""
+# IMPORTS:
+
+
+# FUNCTIONS:
+# Matrix:
+def matrix():
+    mat = []
+    for i in range(6):
+        m = []
+        for j in range(7):
+            m.append(0)
+        mat.append(m)
+    return mat
+
+# Display:
+
+# Check functions:
+
+# AI: 
+
+# Main:
+
+
+# BODY:
+
